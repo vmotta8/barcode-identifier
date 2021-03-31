8317b3c8da9d20b1f712546d3eb791ac7393fa4e
Vinicius Motta <viniciusmotta8@gmail.com>
2021-03-31 17:23:26 -0300

    chore: add timeout

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Gsearch from './identifier'
-
-async function run(barcode: string): Promise<void> {
-    const name = await Gsearch.CodeToName(barcode)
-    console.log(name)
-}
-
-run('9780099477464')
